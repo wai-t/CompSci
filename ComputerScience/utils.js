7cbe60e202171ff6b6cad423db0e4df2ad39a9ab
TSANG <wai@tsangfamily.org>
2024-05-04 20:11:00 +0100

    Add insertion sort, bubble sort, quicksort, and mergesort

--- ComputerScience/utils.js
+++ ComputerScience/utils.js
@@ -123,6 +123,70 @@ function showAlgorithmName(algorithmName) {
     algorithmDiv.innerText = algorithmName;
 }
 
+
+
+let mergeRangeElems = null;
+
+function showMergeRange(start, sorted, divider, length) {
+    console.log(`showMergeRange ${start}, ${sorted}, ${divider}, ${length}`);
+    if (!mergeRangeElems) {
+        mergeRangeElems = {
+            arrayDiv: document.getElementById("array"),
+            sortedSeg: document.createElement("div"),
+            leftSeg: document.createElement("div"),
+            rightSeg: document.createElement("div"),
+        }
+        mergeRangeElems.sortedSeg.setAttribute("class", "range_element sorted");
+        mergeRangeElems.sortedSeg.textContent = "Sorted";
+        mergeRangeElems.arrayDiv.appendChild(mergeRangeElems.sortedSeg);
+        mergeRangeElems.leftSeg.setAttribute("class", "range_element left");
+        mergeRangeElems.leftSeg.textContent = "Left";
+        mergeRangeElems.arrayDiv.appendChild(mergeRangeElems.leftSeg);
+        mergeRangeElems.rightSeg.setAttribute("class", "range_element right");
+        mergeRangeElems.rightSeg.textContent = "Right";
+        mergeRangeElems.arrayDiv.appendChild(mergeRangeElems.rightSeg);    
+    }
+    mergeRangeElems.sortedSeg.style.left = intToPxPos ( (start) * 50 );
+    mergeRangeElems.sortedSeg.style.width = intToPxPos ( sorted * 50 );
+    mergeRangeElems.sortedSeg.style.visibility = (sorted<=0) ? "hidden" : "visible";
+    mergeRangeElems.leftSeg.style.left = intToPxPos ( (start + sorted) * 50 );
+    mergeRangeElems.leftSeg.style.width = intToPxPos ( (divider - sorted) * 50 );
+    mergeRangeElems.leftSeg.style.visibility = (divider <= sorted) ? "hidden" : "visible";
+    mergeRangeElems.rightSeg.style.left = intToPxPos ( (start + divider) * 50 );
+    mergeRangeElems.rightSeg.style.width = intToPxPos ( (length - divider) * 50 );
+    mergeRangeElems.rightSeg.style.visibility = (length <= divider) ? "hidden" : "visible";
+
+    return new Promise((resolve) => setTimeout(()=>resolve(),500));
+}
+
+let pivotNo = 0;
+function pivotElement(currentPivot) {
+    pivotNo++;
+    console.log(`pivotElement ${pivotNo} at ${currentPivot}`);
+    let pivotElement = document.createElement("div");
+    pivotElement.setAttribute("class", "pivot_element");
+    let arrayDiv = document.getElementById("array");
+    pivotElement.textContent = `P${pivotNo}`;
+    pivotElement.style.top = "100px"
+    pivotElement.style.left = intToPxPos (currentPivot * TILE_SIZE)
+    pivotElement.style.visibility = "hidden";
+    arrayDiv.appendChild(pivotElement);
+    return pivotElement;
+}
+
+function placePivotElement(elem, pivotPosition) {
+    if (elem === null) {
+        elem = pivotElement(pivotPosition);
+    }
+    elem.style.visibility = "visible";
+    let path = [
+        [pxPosToInt(elem.style.left), pxPosToInt(elem.style.top)],
+        [(pivotPosition * TILE_SIZE), pxPosToInt(elem.style.top)]
+    ]
+    console.log(`placePivotElement ${path}`);
+    return new Promise(resolve=>elementPath(elem, [...path], resolve));
+}
+
 function doSwap(i,j) {
     let xi = datastore[i];
     let xj = datastore[j];
@@ -138,41 +202,60 @@ function doSwap(i,j) {
     animationBuffer.push(() => {return swapElements(ei,ej)});
 }
 
-function doRotate(i,j, dir=1) {
-    if (i===j)
+/**
+ * doRotate
+ * Move element at "source" to "destination" and shift any elements in between one place towards "source"
+ * @param {*} destination 
+ * @param {*} source 
+ * @returns 
+ */
+function doRotate(destination,source) {
+    if (destination===source)
         return;
-    [i,j] = [Math.min(i,j,), Math.max(i,j)];
-    if (dir>0) {
-        let top = pxPosToInt(document.getElementById(elemIds[i]).style.top);
-        let destX = i * TILE_SIZE;
-        let elem = document.getElementById(elemIds[j]);
-        let path = [[j * TILE_SIZE, top],
-                    [j * TILE_SIZE, (top - TILE_SIZE)]];
-        animationBuffer.push(()=>{return translateElement(elem, [...path]);});
-        let ii = i;
-        let jj = j;
-        for (;ii<jj;jj--) {
-            let elem = document.getElementById(elemIds[jj-1]);
-            let path = [[((jj-1) * TILE_SIZE), top],[( jj * TILE_SIZE ), top]];
-            animationBuffer.push(()=>{return translateElement(elem, path);});
-        }
-        let path2 = [[j * TILE_SIZE, (top - TILE_SIZE)],
-                [destX, (top - TILE_SIZE)],
-                [destX, top]];
-        animationBuffer.push(()=>{return translateElement(elem, path2);});
-        let tempd = datastore[j];
-        let tempe = elemIds[j];
-        for (;i<j;j--) {
-            datastore[j] = datastore[j-1];
-            elemIds[j] = elemIds[j-1];
-        }
-        datastore[i] = tempd;
-        elemIds[i] = tempe;
+
+    dir = destination<source ? -1 : +1;
+
+    let top = pxPosToInt(document.getElementById(elemIds[destination]).style.top);
+    let destX = destination * TILE_SIZE;
+    let elem = document.getElementById(elemIds[source]);
+    let path = [
+        [source * TILE_SIZE, top],
+        [source * TILE_SIZE, (top + TILE_SIZE)]
+    ];
+    animationBuffer.push(()=>{return translateElement(elem, path);});
+    let jj = source;
+    for (; dir*destination > dir*jj; jj+=dir) {
+        let elem = document.getElementById(elemIds[jj+dir]);
+        let path = [
+            [((jj+dir) * TILE_SIZE), top],
+            [( jj * TILE_SIZE ), top]
+        ]
+        animationBuffer.push(()=>{return translateElement(elem, path);});
     }
+    let path2 = [
+        [source * TILE_SIZE, (top + TILE_SIZE)],
+        [destX, (top + TILE_SIZE)],
+        [destX, top]
+    ];
+    animationBuffer.push(()=>{return translateElement(elem, path2);});
+    let tempd = datastore[source];
+    let tempe = elemIds[source];
+    for (;dir*destination>dir*source;source+=dir) {
+        datastore[source] = datastore[source+dir];
+        elemIds[source] = elemIds[source+dir];
+    }
+    datastore[destination] = tempd;
+    elemIds[destination] = tempe;
 }
 
-function animatePointer(pointerElem, x, row) {
-    animationBuffer.push(() => {return translateElement(pointerElem,[[(x-1)*TILE_SIZE, row*TILE_SIZE],[x*TILE_SIZE,row*TILE_SIZE]])});
+function animatePointer(pointerElem, fromX, toX,  row) {
+    animationBuffer.push(() => {return translateElement(pointerElem,[[fromX * TILE_SIZE, row * TILE_SIZE],[toX * TILE_SIZE, row * TILE_SIZE]])});
+}
+
+async function prepareSort() {
+    await cancelAnimation();
+    animationBuffer = [];
+    displayData();
 }
 
 async function cancelAnimation() {
@@ -212,10 +295,16 @@ function playAnimation() {
     p = p.finally(()=>{animationState = "stopped";})
 }
 
-async function reset() {
-    await cancelAnimation();
-    animationBuffer = [];
+async function randomise() {
     generateSourceData(datastore_size);
+    reset();
+}
+async function reset() {
+    if (datastore.length<=0)
+        generateSourceData(datastore_size);
+    await cancelAnimation();
+    mergeRangeElems = null;
+    animationBuffer = [];
     populateDatastore();
     displayData();
 }
